4d44395b9764b2be685dd0fd033f52dfea706714
Richard Barnes <rlb@ipv.sx>
2014-12-15 18:06:16 -0500

    Some minor changes

--- demo.js
+++ demo.js
@@ -2,13 +2,16 @@ var acme = require("acme");
 
 acme.enableLocalUsage();
 
-var server = acme.createServer();
-server.listen(5000);
-console.log("Server listening on port 5000");
+const ENABLE_SERVER = false;
+if (ENABLE_SERVER) {
+  var server = acme.createServer();
+  server.listen(5000);
+  console.log("Server listening on port 5000");
+}
 
-var url = "http://localhost:5000/";
+var url = "http://localhost:4000/acme";
 acme.getMeACertificate(url, "example.com", function(x) {
   console.log("Result of getMeACertificate:");
   console.log(x);
-  server.close();
+  if (ENABLE_SERVER) { server.close(); }
 });
